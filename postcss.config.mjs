6e4c2549842889b1e1e9740d7022d266ce99c936
DevOsa <osadchenko.serghey@icloud.com>
2024-08-31 20:11:54 +0300

    Upd PostCss Add Base Config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
+    'postcss-short': {},
+    'postcss-simple-vars': {},
+    'postcss-mixins': {},
+    'postcss-discard-comments': {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    'postcss-merge-rules': {},
+    'postcss-combine-duplicated-selectors': {},
+    'postcss-sorting': {},
     tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? {cssnano: {}} : {}),
   },
 };
 
