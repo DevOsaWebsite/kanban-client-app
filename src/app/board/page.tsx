e410ca31803366a381c7c517b461839a1d181c60
DevOsa <osadchenko.serghey@icloud.com>
2024-08-31 22:43:15 +0300

    Initial BoardPage

---
+++ src/app/board/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BoardPage = (): React.JSX.Element => {
+  console.log('BoardPage');
+
+  return (
+    <main className='w-full h-full flex flex-col items-center justify-center gap-4 py-40'>
+      <h1>Board</h1>
+    </main>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default BoardPage;
